c490cdd0ecec156bf694c6249c05ec622a5c35eb
Mario Torres <scaramuzzini@gmail.com>
2025-10-29 19:58:56 -0400

    Mapa adicionado com leafleat

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 function Starlink() {
     const [satelites,setSatelites] = useState([]);
@@ -19,17 +21,21 @@ function Starlink() {
     },[]);
     
 
+    const ocean = [-3.0925454075226755, -60.0185281];
 
     return (
         <>
             <h1>Lista de Satélites starlink. Total: {satelites.length}</h1>
-            <ul>
+            {/* <ul>
                 {satelites
                     .filter((sat) => sat.latitude && sat.longitude)
                     .map((sat) => (
                     <li key={sat.id}>{sat.spaceTrack.OBJECT_NAME} LAT {sat.latitude}  LONG{sat.longitude}</li>
                 ))}
-            </ul>
+            </ul> */}
+            <MapContainer center={ocean} zoom={17} scrollWheelZoom={false} style={{height: '70vh'}}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </MapContainer>
         </>
     )
 }
