5ac6d136091fab514539da33ee71b6567469d2fc
Mario Torres <scaramuzzini@gmail.com>
2025-10-29 19:17:20 -0400

    axios request para API starlink

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -1,4 +1,20 @@
+import axios from 'axios';
+
 function Starlink() {
+
+    axios.post('https://api.spacexdata.com/v4/starlink/query', 
+    {
+        "query": {},
+        "options": { limit:10 }
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
     return (
         <h1>Lista de Satélites starlink</h1>
     )
