f48c5f323556738690ad495cbdf9baa918f4cd6a
Mario Torres <scaramuzzini@gmail.com>
2025-10-29 19:47:27 -0400

    filtrando satelites

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -1,22 +1,36 @@
 import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function Starlink() {
+    const [satelites,setSatelites] = useState([]);
 
-    axios.post('https://api.spacexdata.com/v4/starlink/query', 
-    {
-        "query": {},
-        "options": { limit:10 }
-    })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+    useEffect(()=> {
+
+        const fetchSatelites = async () => {
+            const response = await axios.post('https://api.spacexdata.com/v4/starlink/query', 
+            {
+                "query": {},
+                "options": { limit:100 }
+            });
+            setSatelites(response.data.docs);
+            console.log(response.data);
+        }
+        fetchSatelites();
+    },[]);
+    
 
 
     return (
-        <h1>Lista de Satélites starlink</h1>
+        <>
+            <h1>Lista de Satélites starlink. Total: {satelites.length}</h1>
+            <ul>
+                {satelites
+                    .filter((sat) => sat.latitude && sat.longitude)
+                    .map((sat) => (
+                    <li key={sat.id}>{sat.spaceTrack.OBJECT_NAME} LAT {sat.latitude}  LONG{sat.longitude}</li>
+                ))}
+            </ul>
+        </>
     )
 }
 
